6d1e1661a9773f7a5b553b0d4db042cbd851effd
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-08-11 19:26:31 -0300

    Create a imcBox

--- front-end/src/pages/HomeScreen.tsx
+++ front-end/src/pages/HomeScreen.tsx
@@ -3,10 +3,10 @@ import styles  from  './styles.module.css'
 const HomeScreen = () =>{
    return(
     <div className={styles.imcBox}>
-        <label>Weight</label>
-        <input className={styles.input} type="number" name="" id="" />
-        <label>Height</label>
-        <input className={styles.input} type="number" name="" id="" />
+        <label className={styles.labelInput} >Weight</label>
+        <input className={styles.input} placeholder='  Enter your weight' type="number" name="" id="" />
+        <label className={styles.labelInput}>Height</label>
+        <input className={styles.input} placeholder='  Enter your height' type="number" name="" id="" />
         <button className={styles.btn} >Calculate</button>
     </div>
    )
