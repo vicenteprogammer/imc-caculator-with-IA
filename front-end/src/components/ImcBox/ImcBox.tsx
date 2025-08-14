815a47f4b4bce62a00b4eb8c6935537ddac9aec7
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-08-14 15:12:09 -0300

    Increment new interface of apge

--- front-end/src/components/ImcBox/ImcBox.tsx
+++ front-end/src/components/ImcBox/ImcBox.tsx
@@ -1,12 +1,18 @@
 import styles from './styles.module.css'
 const ImcBox = () =>{
+    
     return(
      <div className={styles.imcBox}>
+        <p className={styles.title} >Imc Calculator</p>
         <label className={styles.labelInput} >Weight</label>
         <input className={styles.input} placeholder=' Enter your weight' type="number" name="" id="" />
         <label className={styles.labelInput}>Height</label>
         <input className={styles.input} placeholder=' Enter your height' type="number" name="" id="" />
-        <button className={styles.btn} >Calculate</button>
+        <div className={styles.btnDiv} >
+         <button className={styles.btn} >Calculate</button>
+         <button className={styles.btn} >Reset</button>
+        </div>
+        
         <p className={styles.subtitle}>Seu imc é 20.10</p>
     </div>
     )
