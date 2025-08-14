b229cd7bcda8d0f7c97dcbd14d1b1b59b1012f89
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-08-14 12:30:57 -0300

    some changes in application

--- front-end/src/components/ResultBox/ResultBox.tsx
+++ front-end/src/components/ResultBox/ResultBox.tsx
@@ -1,12 +1,10 @@
-import ListRecomend from '../ListRecomend/ListRecomend'
+
 import styles from './styles.module.css'
 const ResultBox = () =>{
     return(
         <div className={styles.resultBox}>
             <h1 className={styles.h1} >Diet Sugestion</h1>
             <div className={styles.list}>
-                <ListRecomend/>
-                <ListRecomend/>
 
             </div>
         </div>
--- front-end/src/pages/HomeScreen.tsx
+++ front-end/src/pages/HomeScreen.tsx
@@ -4,7 +4,7 @@ import styles  from  './styles.module.css'
 
 const HomeScreen = () =>{
    return(
-    <><ImcBox /><ResultBox /></>
+    <><ImcBox /></>
    )
 }
 
